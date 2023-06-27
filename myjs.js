fdc5aa325613b5a9a601b26d4d830747b4c7f040
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-06-27 09:29:23 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,7 +1,8 @@
-alart('Hello,osamu!');
+alert('Hello,osamu!');
 alert(1+1);
 alert(math.sqrt(1+1));
 alert(2+3);
 alert('2'+'3');
-alertNumber('2')+Number('3');
-
+alertNumber(('2')+Number('3'));
+alert('2'+3);
+alert(('2')+3);
